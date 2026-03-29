d55e87e7452f140661fc1d0d05cd6b47ca39fb83
jibusang <191512591@qq.com>
2026-03-29 21:22:59 +0800

    feat(ExamSystem): 新增类初始化方法及学生数据加载框架

--- system.py
+++ system.py
@@ -1,19 +1,38 @@
-# 导入允许的标准库
+
 import os
 import random
 import time
 
-# 学生实体类 空定义
+
+#Student数据类、__init__、__str__、
 class Student:
-    def __init__(self):
+    # 初始化属性
+    def __init__(self, name, gender, cls, sid, college):
+        self.name = name
+        self.gender = gender
+        self.cls = cls
+        self.student_id = sid
+        self.college = college
+
+
+    def __str__(self):
+        return f"姓名:{self.name} 性别:{self.gender} 班级:{self.cls} 学号:{self.student_id} 学院:{self.college}"
+
+
+    @staticmethod
+    def check_student_id(sid):
+        return sid.isdigit()
+
+
+# 系统管理类
+class ExamSystem:
+    def __init__(self, filename):
+        self.filename = filename
+        self.students = []
+
+    def load_students(self):
         pass
 
-# 考场系统主类 空定义
-class ExamSystem:
-    def __init__(self, file_name):
-        self.file_name = file_name
-        self.student_list = []
 
-# 程序入口启动
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = ExamSystem("人工智能编程语言学生名单.txt")
